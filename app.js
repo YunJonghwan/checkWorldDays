e0b4953523e87e09198f4b86d74a2883d93d5170
윤종환 <yunjonghwan99@gmail.com>
2025-03-06 10:07:50 +0900

    handlebars를 활용해서 이전의 index.html을 표시하도록 layout과 partials를 나누고 설정해줬다.

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express');
-const path = require('path');
+const { engine } = require('express-handlebars');
 
 const app = express();
 
-app.use('/', express.static(path.join(__dirname, '/public')));
+//* 참조할 파일 경로 지정
+app.use('/', express.static(__dirname + '/public'));
+
+//* handlebars를 사용하기 위한 설정
+app.engine('handlebars', engine({
+  //* 기본 레이아웃은 main으로 지정
+  defaultLayout: "main.handlebars",
+  //* 레이아웃 폴더 위치 지정
+  layoutsDir: __dirname + '/public/view/layouts',
+  //* paritals 폴더 경로 지정 재사용이 가능하도록 paritals를 따로 만들어준다. 
+  partialsDir: __dirname + '/public/view/partials'
+}));
+//* handlebars 엔진 지정
+app.set('view engine', 'handlebars');
+//* render를 실행할 때 참조할 경로 지정
+app.set('views', './public/view');
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/public/view/index.html');
+  res.render('home');
 });
 
 app.listen(3000, () => {
